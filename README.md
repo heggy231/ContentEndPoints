207a66a59118572547a8fb88c8db33766c5cf21e
Heggyhere <heggyy@gmail.com>
2018-09-28 19:29:01 -0700

    render thumbnail

--- README.md
+++ README.md
@@ -1,24 +1,57 @@
 # Expense organizer app
 
 Expense organizer app that runs on DBX platform.  Dropbox integration using the Dropbox APIs featuring content Endpoints Dropbox API.
-  - Method: files/get_thumbnail Dropbox API call to return a jpeg image for a given path
+  - OAuth: Direct the user to Dropbox, and prompt for a code.
+  - Method: Files/get_thumbnail Dropbox API call to return a jpeg image for a given path
 
 
-## What do I do from here?
-- Option 1
-1. Check for Python installation, if Python is missing or not found, you’ll need to **[install](https://www.python.org/downloads/)** it here.
+## What do I do to try out OAuth on DBX?
+- OAuth
+=======
+1. Check for Python installation in your terminal, if Python is missing or not found, you’ll need to **[install](https://www.python.org/downloads/)** it here.
 ------
 
 > python -V
 
 
 ------
-2. CHOOSE FOLDER to your site file local repo.
-3. Check mark on Options: Accessible on local network
-   Check mark on Auto show index.html
-   Enter Port: 8887
-   Set CORS headers
-4. Set your URL to http://127.0.0.1:8887
+2. Install DropBox (DBX) Platform Python SDK.
+------
+
+> pip install dropbox
+
+------
+
+3. OAuth, direct the user to Dropbox, and prompt for a code.  DropboxOAuth2FlowNoRedirect object constructs a URL to https://www.dropbox.com/oauth2/authorize
+
+------
+
+> from dropbox import DropboxOAuth2FlowNoRedirect
+>
+> auth_flow = DropboxOAuth2FlowNoRedirect("APP_KEY", "APP_SECRET")
+> print("Authorize your script here: "+auth_flow.start())
+> code = raw_input('Input your code: ').strip()
+> 
+> print("Initializing Dropbox API...")
+> dbx = dropbox.Dropbox(auth_flow.finish(code).access_token)
+
+------
+
+4. Run db1.py in your terminal
+
+5. Go to the URL you are given to get the OAuth code to get access to the expense receipts
+
+## What do I do to try out the get image thumbnail on DBX?
+- thumbnail render
+==================
+1. Check for Python installation in your terminal, if Python is missing or not found, you’ll need to **[install](https://www.python.org/downloads/)** it here.
+------
+
+> python -V
+
+
+------
+
 
 ## References: 
 - [Note](https://goo.gl/Uovijx)
---
+++ db1.py
@@ -0,0 +1,93 @@
+import dropbox
+import posixpath
+from dropbox import DropboxOAuth2FlowNoRedirect
+import sys 
+
+auth_flow = DropboxOAuth2FlowNoRedirect("y9r9coydrlppgmc", "qfapyf1vjfmvhk5")
+print("Authorize your script here: "+auth_flow.start())
+code = raw_input('Input your code: ').strip()
+
+print("Initializing Dropbox API...")
+dbx = dropbox.Dropbox(auth_flow.finish(code).access_token)
+
+# print("Initializing Dropbox API...")
+# dbx = dropbox.Dropbox("_g3rh767bdUAAAAAAAAQ3JIPgpil68H_3RhDCBZvIaUqraTL6lb6VA2xOfK4SN9t")
+
+
+# for entry in result.entries:
+#     print(entry.name+", modified "+entry.client_modified.strftime('%Y-%m-%d'))
+
+
+def process_folder_entries(current_state, entries):
+    for entry in entries: # loop that goes through entries
+        if isinstance(entry, dropbox.files.FileMetadata): # FileMetadata is state update about file
+            current_state[entry.path_lower] = entry
+        elif isinstance(entry, dropbox.files.DeletedMetadata):
+            current_state.pop(entry.path_lower, None) # ignore KeyError if missing
+    return current_state
+
+print("Scanning for expense files...")
+result = dbx.files_list_folder(path="/Sample Expenses", limit=5)
+files = process_folder_entries({}, result.entries)
+
+for entry in files.values():
+    print(entry.name + ", modified "+entry.client_modified.strftime('%Y-%m-%d'))
+# The result may be different order if that matters add sorting step
+
+## 
+
+print("Scanning for expense files...")
+result = dbx.files_list_folder(path="/Sample Expenses", limit=5)
+files = process_folder_entries({}, result.entries)
+
+# follow the cursor until we've collected all results
+while result.has_more:
+    print("Collecting additional files...")
+    result = dbx.files_list_folder_continue(result.cursor)
+    files = process_folder_entries(files, result.entries)
+
+
+#########
+for entry in files.values():
+    # use modified time of file to build destination path
+    destination_path = posixpath.join(
+        "/" + str(entry.client_modified.year) + "_Expenses",
+        str(entry.client_modified.month),
+        entry.name
+    )
+    print("Moving {} to {}".format(entry.path_display, destination_path))
+    dbx.files_move_v2(entry.path_lower, destination_path)
+
+
+
+# python 2x & 3x compatability
+if sys.version_info[0] >= 3:
+    from http.server import BaseHTTPRequestHandler,HTTPServer
+    def print_bytes(str):
+        return bytes(str, 'utf-8')
+else:    
+    from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+    def print_bytes(str):
+        return bytes(str)
+
+port = 8088
+root = "http://localhost"
+
+class MyApp(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()            
+        self.wfile.write(print_bytes('<html><body>Hello, world</body></html>'))
+  
+def run(server_class=HTTPServer, handler_class=MyApp, port=port):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print('Server running at '+root+':'+str(port)+'...')
+    httpd.serve_forever()
+  
+run()
+
+
+
+# Notes: https://paper.dropbox.com/doc/SF-DBX-Dev-Workshop-Guide-Getting-Started-with-the-Dropbox-API--ANRhVct9g4DcCwi1XJx5QGmxAg-CQExilGxVruQh0EO2mdJ6
